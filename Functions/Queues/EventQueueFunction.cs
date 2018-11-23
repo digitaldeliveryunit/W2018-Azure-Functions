97d67e3f75bff4efb02c5de19958cbe2f9290445
Thuong Nguyen <nguyen.huythuon@petronas.com>
2018-11-23 17:39:31 +0800

    Fix some errors in EventQueueFunction.

---
+++ Functions/Queues/EventQueueFunction.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Threading.Tasks;
+using com.petronas.myevents.api.Constants;
+using com.petronas.myevents.api.Services.Interfaces;
+using com.petronas.myevents.api.ViewModels;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Willezone.Azure.WebJobs.Extensions.DependencyInjection;
+
+namespace com.petronas.myevents.api.Functions.Queues
+{
+    public static class EventQueueFunction
+    {
+        [FunctionName("EventQueueFunction")]
+        public static async Task Run(
+            [QueueTrigger(
+                QueueNames.Events,
+                Connection = AppSettings.StorageConnectionString)]string queueMessage,
+            ILogger log,
+            [Inject]IEventService eventService)
+        {
+            var message = JsonConvert.DeserializeObject<QueueMessage>(queueMessage);
+            var queueType = Enum.Parse(typeof(QueueType), message.QueueType);
+            if (message != null)
+            {
+                switch (queueType)
+                {
+                    case QueueType.BOOKMARK:
+                        await eventService.Bookmark(message.EventId, message.UserId);
+                        break;
+                    case QueueType.UNBOOKMARK:
+                        await eventService.UnBookmark(message.EventId, message.UserId);
+                        break;
+                    case QueueType.JOIN:
+                        // await eventService.Join(message.EventId, message.UserId);
+                        break;
+                    case QueueType.UN_JOIN:
+                        // await eventService.UnJoin(message.EventId, message.UserId);
+                        break;
+                }
+            }
+            else
+            {
+                log.LogError($"The message cannot be parsed");
+            }
+        }
+    }
+}
